1b78df5ce0365f91c1dc9027cfa275c61c156480
Dorian Devua <Dorian.DevUa@gmail.com>
2024-05-12 00:59:06 +0400

    add some changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './constants';
-import { App } from './components/App';
 import { GlobalStyle } from './styles';
+import { App } from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
