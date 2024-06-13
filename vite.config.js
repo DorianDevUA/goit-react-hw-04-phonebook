85bf7bc6087240769fb78a9978320b196be70e42
Dorian Devua <Dorian.DevUa@gmail.com>
2024-06-13 22:32:46 +0400

    change vite config and project name

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
-  base: 'goit-react-hw-03-phonebook-formik',
+  base: 'goit-react-hw-04-phonebook',
   build: {
     sourcemap: true,
   },
